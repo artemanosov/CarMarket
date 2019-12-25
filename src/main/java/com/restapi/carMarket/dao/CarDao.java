7c096fc24343b42355ceb6938d590c13c312fd51
artemanosov <50707957+artemanosov@users.noreply.github.com>
2019-12-24 21:05:55 -0500

    Update CarDao.java

--- src/main/java/com/restapi/carMarket/dao/CarDao.java
+++ src/main/java/com/restapi/carMarket/dao/CarDao.java
@@ -2,9 +2,8 @@ package com.restapi.carMarket.dao;
 
 import com.restapi.carMarket.model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface CarDao extends JpaRepository<Car,Long> {
 
 }
